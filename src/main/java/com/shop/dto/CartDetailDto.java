16acc3ad22504add71b6417a83e8bd5aa0869fb5
parkjieun <jieunii0@naver.com>
2023-11-16 01:07:29 +0900

    장바구니조회하기

---
+++ src/main/java/com/shop/dto/CartDetailDto.java
@@ -0,0 +1,31 @@
+package com.shop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CartDetailDto {
+    // 장바구니 상품 아이디
+    private Long cartItemId;
+
+    // 상품명
+    private String itemNm;
+
+    // 상품 금액
+    private int price;
+
+    // 수량
+    private int count;
+
+    // 상품 이미지 경로
+    private String imgUrl;
+
+    public CartDetailDto(Long cartItemId, String itemNm, int price, int count, String imgUrl) {
+        this.cartItemId = cartItemId;
+        this.itemNm = itemNm;
+        this.price = price;
+        this.count = count;
+        this.imgUrl = imgUrl;
+    }
+}
